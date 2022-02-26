7804c6591caa81a3a30b7f8f962a58a1b4df1159
摄氏零度AY <13453157350@163.com>
2022-02-26 16:45:49 +0800

    修改配置

--- huaweifs/src/test/groovy/HuaweiFileStorageTest.groovy
+++ huaweifs/src/test/groovy/HuaweiFileStorageTest.groovy
@@ -1,3 +1,4 @@
+import io.github.pierresj.huaweifs.HuaweiFileStorage
 import io.jmix.core.CoreConfiguration
 import io.jmix.core.FileRef
 import io.jmix.core.FileStorage
@@ -22,50 +23,24 @@ class HuaweiFileStorageTest extends Specification {
     @Autowired
     private FileStorage fileStorage
 
-
-    def "save stream"(){
+    def "save open remove"(){
         def fileName=UuidProvider.createUuid().toString()+".txt";
-        def fileStream=this.getClass().getClassLoader().getResourceAsStream("files/simple.txt");
-        def fileRef=fileStorage.saveStream(fileName,fileStream);
+        String string = "Text for testing.";
+        InputStream inputStream = new ByteArrayInputStream(string.getBytes());
+        def fileRef=fileStorage.saveStream(fileName,inputStream);
+        def fileExists= fileStorage.fileExists(fileRef)
         def openedStream=fileStorage.openStream(fileRef);
+        def fileOpened =openedStream!=null
+        fileStorage.removeFile(fileRef)
         expect:
-            openedStream!=null
+        verifyAll {
+            fileExists
+            fileOpened
+        }
     }
 
-    def "openStream"(){
-        def fileKey = "2022/02/24/449321b3-7dd5-c05c-1d97-d4780528f624.txt"
-        def fileName="449321b3-7dd5-c05c-1d97-d4780528f624.txt"
-        def storageName = fileStorage.getStorageName()
-        def fileRef = new FileRef(storageName, fileKey, fileName)
-        fileStorage.openStream(fileRef)
-
-        expect: true
+    def "huawei storage initialized"() {
+        expect:
+        fileStorage.getStorageName() == HuaweiFileStorage.DEFAULT_STORAGE_NAME
     }
-
-    def "fileExists"() {
-        def storageName = fileStorage.getStorageName()
-        def fileKey = "2022/02/24/449321b3-7dd5-c05c-1d97-d4780528f624.txt"
-        def fileName="449321b3-7dd5-c05c-1d97-d4780528f624.txt"
-
-        def fileref = new FileRef(storageName, fileKey, fileName)
-        def exists = fileStorage.fileExists(fileref)
-
-        expect:  exists
-
-    }
-
-
-    def "removeFile"(){
-        def storageName = fileStorage.getStorageName()
-        def fileKey = "2022/02/24/2480b1c7-c5fc-c212-3ec4-03d2d6351b90.txt "
-        def fileName="2480b1c7-c5fc-c212-3ec4-03d2d6351b90.txt "
-
-        def fileref = new FileRef(storageName, fileKey, fileName);
-        fileStorage.removeFile(fileref)
-
-        def exists = fileStorage.fileExists(fileref)
-
-        expect:  !exists
-    }
-
 }
